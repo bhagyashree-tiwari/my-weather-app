1bb235700d14d76604edbfbc5db1985fd12221c3
bvt12 <tiwarib1206@gmail.com>
2021-03-25 18:38:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
 
 function App() {
+  const [lat, setLat] = useState();
+  const [long, setLong] = useState();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      }, function () {
+        alert("Allow location access to continue.");
+      });
+
+      console.log("Latitude is:", lat);
+      console.log("Longitude is:", long);
+
+      if (lat !== undefined && long !== undefined) {
+        await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`, {
+          method: "GET",
+        }).then(res => res.json())
+          .then(result => {
+            setData(result)
+            console.log(result);
+            console.log("data: " + data);
+          });
+      }
+    }
+    fetchData();
+  }, [lat, long]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
